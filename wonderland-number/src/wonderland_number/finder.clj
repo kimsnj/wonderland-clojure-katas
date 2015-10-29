009c4efd652d90188f9b72a9fb17b3e77e34baae
Karim Snj <karim.snj@gmail.com>
2015-10-29 22:05:50 +0100

    Solution for wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,12 @@
 (ns wonderland-number.finder)
 
+(defn isWonderNum? [n]
+  (let [digit-set #(set (str %))
+        digits (digit-set n)
+        mult-digits (map (comp digit-set (partial * n)) [2 3 4 5 6])]
+    (every? (partial = digits) mult-digits)))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (->> (range 100000 999999)
+       (filter isWonderNum?)
+       first))
