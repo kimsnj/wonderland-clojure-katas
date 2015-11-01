0decdeba24748a4f83d88291c25efccb86eedffe
Karim S. <karimair@gmail.com>
2015-11-01 18:32:17 +0100

    Solution for tiny maze

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -1,3 +1,32 @@
 (ns tiny-maze.solver)
 
-(defn solve-maze [maze])
+(def moves [[1 0] [0 1] [-1 0] [0 -1]])
+
+(defn is-possible-position [maze [x y]]
+  ((complement contains?)
+     #{:x 1 nil}
+     (nth (nth maze x []) y nil)))
+
+(defn possible-positions [maze pos]
+  (->> moves
+       (map (partial map + pos))
+       (filter (partial is-possible-position maze))))
+
+(defn visit-maze [maze [x y]]
+  (assoc maze x (assoc (maze x) y :x)))
+
+(defn is-exit-found [maze [x y]]
+  (= ((maze x) y) :E))
+
+(def first-truthy
+  (comp first (partial filter identity)))
+
+(defn walk [maze pos]
+  (let [new-maze (visit-maze maze pos)
+        next-moves (possible-positions maze pos)]
+    (cond (is-exit-found maze pos) new-maze
+          (empty? next-moves) nil
+          :else (first-truthy (map (partial walk new-maze) next-moves)))))
+
+(defn solve-maze [maze]
+  (walk maze '(0 0)))
