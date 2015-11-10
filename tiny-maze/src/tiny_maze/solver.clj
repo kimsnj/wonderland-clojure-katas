f4c73e76eaeaf7bddd6706f1e4b235add7a74b43
Karim Snj <karim.snj@gmail.com>
2015-11-10 19:58:52 +0100

    tiny-maze:using core function to simplify solution

--- tiny-maze/src/tiny_maze/solver.clj
+++ tiny-maze/src/tiny_maze/solver.clj
@@ -2,24 +2,24 @@
 
 (def moves [[1 0] [0 1] [-1 0] [0 -1]])
 
-(defn is-possible-position [maze [x y]]
+(defn is-possible-position [maze pos]
   ((complement contains?)
      #{:x 1 nil}
-     (nth (nth maze x []) y nil)))
+     (get-in maze pos)))
 
 (defn possible-positions [maze pos]
   (->> moves
        (map (partial map + pos))
        (filter (partial is-possible-position maze))))
 
-(defn visit-maze [maze [x y]]
-  (assoc maze x (assoc (maze x) y :x)))
+(defn visit-maze [maze pos]
+  (assoc-in maze pos :x))
 
 (defn is-exit-found [maze [x y]]
   (= ((maze x) y) :E))
 
 (def first-truthy
-  (comp first (partial filter identity)))
+  (partial some identity))
 
 (defn walk [maze pos]
   (let [new-maze (visit-maze maze pos)
